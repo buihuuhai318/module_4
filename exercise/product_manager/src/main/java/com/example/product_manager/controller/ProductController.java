ee3f559695a4e39c4833aae221e64d487b833e6c
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-08-16 20:54:23 +0700

    new

--- exercise/product_manager/src/main/java/com/example/product_manager/controller/ProductController.java
+++ exercise/product_manager/src/main/java/com/example/product_manager/controller/ProductController.java
@@ -2,14 +2,10 @@ package com.example.product_manager.controller;
 
 import com.example.product_manager.model.Product;
 import com.example.product_manager.service.IProductService;
-import com.example.product_manager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -72,5 +68,18 @@ public class ProductController {
     public String view(@PathVariable int id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
         return "view";
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam String name, Model model) {
+        List<Product> productList = new ArrayList<>(productService.listProduct().values());
+        List<Product> productListTemp = new ArrayList<>();
+        for (Product product : productList) {
+            if (product.getName().contains(name)) {
+                productListTemp.add(product);
+            }
+        }
+        model.addAttribute("productListTemp", productListTemp);
+        return "search";
     }
 }
