d27ca9c2df64a6400d19108dae3a1a7c75a5ddc6
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-08-22 15:34:40 +0700

    new

--- exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/repository/IBlogRepository.java
+++ exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/repository/IBlogRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IBlogRepository extends JpaRepository<Blog, Long> {
-    Page<Blog> findBlogByTitleContaining(Pageable pageable, String searchName);
+    Page<Blog> findBlogByTitleContaining(Pageable pageable, String searchTitle);
 }
