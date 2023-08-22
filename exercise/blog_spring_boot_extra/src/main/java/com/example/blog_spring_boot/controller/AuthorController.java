bc1f4be2b5df03468fd4e68fae88d4044f092385
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-08-22 11:13:58 +0700

    new

--- exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/controller/AuthorController.java
+++ exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/controller/AuthorController.java
@@ -9,10 +9,7 @@ import com.example.blog_spring_boot.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
@@ -42,5 +39,12 @@ public class AuthorController {
     public String listAuthors(Model model) {
         model.addAttribute("authors", authorService.findAll());
         return "authors";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteAuthor(@PathVariable long id, RedirectAttributes redirectAttributes) {
+        authorService.remove(id);
+        redirectAttributes.addFlashAttribute("message", "Author Deleted Successfully!");
+        return "redirect:/authors";
     }
 }
--- exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/controller/CategoryController.java
+++ exercise/blog_spring_boot_extra/src/main/java/com/example/blog_spring_boot/controller/CategoryController.java
@@ -9,10 +9,7 @@ import com.example.blog_spring_boot.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
@@ -42,5 +39,12 @@ public class CategoryController {
     public String listCategories(Model model) {
         model.addAttribute("categories", categoryService.findAll());
         return "categories";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable long id, RedirectAttributes redirectAttributes) {
+        categoryService.remove(id);
+        redirectAttributes.addFlashAttribute("message", "Category Deleted Successfully!");
+        return "redirect:/categories";
     }
 }
